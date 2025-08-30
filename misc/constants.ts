f85eeb7fd83aa9d13765096659b7b20e0296321a
Anton Isaev <jcant.gm@gmail.com>
2025-08-30 21:13:15 +0300

    add Pinia module also fix constants

--- misc/constants.ts
+++ misc/constants.ts
@@ -1,8 +1,8 @@
 export const serverApi = {
   GET_ALL_USERS: "/api/user/all",
   GET_USER: "/api/user/",
-  GET_ALL_SHOPPING_LISTS: "/api/shopping/all",
-  GET_SHOPPING_LIST: "/api/shopping/",
+  GET_ALL_SHOPPING_LISTS: "/api/shopping_list/all",
+  GET_SHOPPING_LIST: "/api/shopping_list/",
   TEST: "/api/test",
 };
 
