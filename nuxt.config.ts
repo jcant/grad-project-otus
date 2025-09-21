8e44f4777c1465997e236c7c980edf6b17d0ac24
Anton Isaev <jcant.gm@gmail.com>
2025-09-21 10:56:29 +0300

    enable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   routeRules: {
-    "**": { ssr: false },
+    "**": { ssr: true },
   },
 
   modules: [
